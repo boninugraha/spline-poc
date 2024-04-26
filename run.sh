115ddd0f9be246049bff7427263202f7d025ccca
nugrahacode <boni.nugraha@accenture.com>
2024-04-26 15:31:47 +0700

    Change run.sh to add spline params

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-spark-submit code.py
+spark-submit \
+  --packages za.co.absa.spline.agent.spark:spark-3.0-spline-agent-bundle_2.12:2.0.0 \
+  --conf "spark.sql.queryExecutionListeners=za.co.absa.spline.harvester.listener.SplineQueryExecutionListener" \
+  --conf "spark.spline.producer.url=http://localhost:8081/producer" \
+  code.py 
